482aff13eb9b19e260f93ec5982d8c993afda0e4
Scheidler, Nicolas <Nicolas.Scheidler@ecom-ex.com>
2016-08-04 14:17:26 +0200

    Revert "Added adodb-time Library for mktime replacement"
    This reverts commit 3c557c42a31f8baf2372fa82f26a55627cf954ed.

--- Classes/Controller/NewsController.php
+++ Classes/Controller/NewsController.php
@@ -2,36 +2,14 @@
 
 namespace Cbrunet\CbNewscal\Controller;
 
-    /* Initialization */
-
-/*
-	Version Number
-*/
-define('ADODB_DATE_VERSION',0.35);
-
-$ADODB_DATETIME_CLASS = (PHP_VERSION >= 5.2);
-
-/*
-	This code was originally for windows. But apparently this problem happens
-	also with Linux, RH 7.3 and later!
-
-	glibc-2.2.5-34 and greater has been changed to return -1 for dates <
-	1970.  This used to work.  The problem exists with RedHat 7.3 and 8.0
-	echo (mktime(0, 0, 0, 1, 1, 1960));  // prints -1
-
-	References:
-	 http://bugs.php.net/bug.php?id=20048&edit=2
-	 http://lists.debian.org/debian-glibc/2002/debian-glibc-200205/msg00010.html
-*/
-
-if (!defined('ADODB_ALLOW_NEGATIVE_TS')) define('ADODB_NO_NEGATIVE_TS',1);
-
 class NewsController extends \GeorgRinger\News\Controller\NewsController {
+
 
 	protected $year;
 	protected $month;
 
 	protected $events;
+
 
 	/**
 	 *
@@ -46,7 +24,7 @@ class NewsController extends \GeorgRinger\News\Controller\NewsController {
 		$monthsAfter = (int)$this->settings['monthsAfter'];
 
 		for ($m = $this->month - $monthsBefore; $m <= $this->month + $monthsAfter; $m++) {
-			$cm = $this->adodb_mktime(0, 0, 0, $m, 1, $this->year);
+			$cm = mktime(0, 0, 0, $m, 1, $this->year);
 			$month = date('n', $cm);
 			$year = date('Y', $cm);
 			$months[] = array(
@@ -96,7 +74,7 @@ class NewsController extends \GeorgRinger\News\Controller\NewsController {
 			// Display relative to current month
 			if (strlen($displayMonth) > 1 && ($displayMonth[0] == '-' || $displayMonth[0] == '+')) {
 				$displayMonth = (int)$displayMonth;
-				$mt = $this->adodb_mktime(0, 0, 0, $this->month + $displayMonth, 1, $this->year);
+				$mt = mktime(0, 0, 0, $this->month + $displayMonth, 1, $this->year);
 				$this->year = (int)date('Y', $mt);
 				$this->month = (int)date('n', $mt);
 			}
@@ -197,13 +175,13 @@ class NewsController extends \GeorgRinger\News\Controller\NewsController {
 
 	protected function getWeeks($demand, $month, $year) {
 		$curday = $this->firstDayOfMonth($month, $year);
-		$lastday = date('t', $this->adodb_mktime(0, 0, 0, $month, 1, $year));
+		$lastday = date('t', mktime(0, 0, 0, $month, 1, $year));
 		$weeks = array();
 		while ($curday <= $lastday) {
 			$week = array();
 			for ($d=0; $d<7; $d++) {
-                $dts = $this->adodb_mktime(0, 0, 0, $month, $curday, $year);
 				$day = array();
+				$dts = mktime(0, 0, 0, $month, $curday, $year);
 				$day['ts'] = $dts;
 				$day['day'] = date('j', $dts);
 				$day['month'] = date('n', $dts);
@@ -232,7 +210,7 @@ class NewsController extends \GeorgRinger\News\Controller\NewsController {
 	 * @return int
 	 **/
 	protected function firstDayOfMonth($month, $year) {
-		$fdom = $this->adodb_mktime(0, 0, 0, $month, 1, $year);  // First day of the month
+		$fdom = mktime(0, 0, 0, $month, 1, $year);  // First day of the month
 		$fdow = (int)date('w', $fdom);  // Day of week of the first day
 
 		$fd = 1 - $fdow + $this->settings['firstDayOfWeek'];  // First day of the calendar
@@ -272,10 +250,10 @@ class NewsController extends \GeorgRinger\News\Controller\NewsController {
 				break;
 		}
 
-		$prevdate = $this->adodb_mktime(0, 0, 0, $this->month - $monthsToScroll, 1, $this->year);
+		$prevdate = mktime(0, 0, 0, $this->month - $monthsToScroll, 1, $this->year);
 		if ($this->settings['timeRestriction']) {
 			$ts = strtotime($this->settings['timeRestriction']);
-			$trm = $this->adodb_mktime(0, 0, 0, date('n', $ts), 1, date('Y', $ts));
+			$trm = mktime(0, 0, 0, date('n', $ts), 1, date('Y', $ts));
 			if ($prevdate < $trm) {
 				$navigation['prev'] = NULL;
 			}
@@ -285,10 +263,10 @@ class NewsController extends \GeorgRinger\News\Controller\NewsController {
 			$navigation['prev']['year'] = date('Y', $prevdate);
 		}
 
-		$nextdate = $this->adodb_mktime(0, 0, 0, $this->month + $monthsToScroll, 1, $this->year);
+		$nextdate = mktime(0, 0, 0, $this->month + $monthsToScroll, 1, $this->year);
 		if ($this->settings['timeRestrictionHigh']) {
 			$ts = strtotime($this->settings['timeRestrictionHigh']);
-			$trm = $this->adodb_mktime(0, 0, 0, date('n', $ts), 1, date('Y', $ts));
+			$trm = mktime(0, 0, 0, date('n', $ts), 1, date('Y', $ts));
 			if ($nextdate > $trm) {
 				$navigation['next'] = NULL;
 			}
@@ -303,194 +281,5 @@ class NewsController extends \GeorgRinger\News\Controller\NewsController {
 
 		return $navigation;
 	}
-
-    public function adodb_mktime($hr,$min,$sec,$mon=false,$day=false,$year=false,$is_dst=false,$is_gmt=false)
-    {
-        if (!defined('ADODB_TEST_DATES')) {
-
-            if ($mon === false) {
-                return $is_gmt? @gmmktime($hr,$min,$sec): @mktime($hr,$min,$sec);
-            }
-
-            // for windows, we don't check 1970 because with timezone differences,
-            // 1 Jan 1970 could generate negative timestamp, which is illegal
-            $usephpfns = (1970 < $year && $year < 2038
-                || !defined('ADODB_NO_NEGATIVE_TS') && (1901 < $year && $year < 2038)
-            );
-
-
-            if ($usephpfns && ($year + $mon/12+$day/365.25+$hr/(24*365.25) >= 2038)) $usephpfns = false;
-
-            if ($usephpfns) {
-                return $is_gmt ?
-                    @gmmktime($hr,$min,$sec,$mon,$day,$year):
-                    @mktime($hr,$min,$sec,$mon,$day,$year);
-            }
-        }
-
-        $gmt_different = ($is_gmt) ? 0 : $this->adodb_get_gmt_diff($year,$mon,$day);
-
-        /*
-        # disabled because some people place large values in $sec.
-        # however we need it for $mon because we use an array...
-        $hr = intval($hr);
-        $min = intval($min);
-        $sec = intval($sec);
-        */
-        $mon = intval($mon);
-        $day = intval($day);
-        $year = intval($year);
-
-
-        $year = $this->adodb_year_digit_check($year);
-
-        if ($mon > 12) {
-            $y = floor(($mon-1)/ 12);
-            $year += $y;
-            $mon -= $y*12;
-        } else if ($mon < 1) {
-            $y = ceil((1-$mon) / 12);
-            $year -= $y;
-            $mon += $y*12;
-        }
-
-        $_day_power = 86400;
-        $_hour_power = 3600;
-        $_min_power = 60;
-
-        $_month_table_normal = array("",31,28,31,30,31,30,31,31,30,31,30,31);
-        $_month_table_leaf = array("",31,29,31,30,31,30,31,31,30,31,30,31);
-
-        $_total_date = 0;
-        if ($year >= 1970) {
-            for ($a = 1970 ; $a <= $year; $a++) {
-                $leaf = $this->_adodb_is_leap_year($a);
-                if ($leaf == true) {
-                    $loop_table = $_month_table_leaf;
-                    $_add_date = 366;
-                } else {
-                    $loop_table = $_month_table_normal;
-                    $_add_date = 365;
-                }
-                if ($a < $year) {
-                    $_total_date += $_add_date;
-                } else {
-                    for($b=1;$b<$mon;$b++) {
-                        $_total_date += $loop_table[$b];
-                    }
-                }
-            }
-            $_total_date +=$day-1;
-            $ret = $_total_date * $_day_power + $hr * $_hour_power + $min * $_min_power + $sec + $gmt_different;
-
-        } else {
-            for ($a = 1969 ; $a >= $year; $a--) {
-                $leaf = $this->_adodb_is_leap_year($a);
-                if ($leaf == true) {
-                    $loop_table = $_month_table_leaf;
-                    $_add_date = 366;
-                } else {
-                    $loop_table = $_month_table_normal;
-                    $_add_date = 365;
-                }
-                if ($a > $year) { $_total_date += $_add_date;
-                } else {
-                    for($b=12;$b>$mon;$b--) {
-                        $_total_date += $loop_table[$b];
-                    }
-                }
-            }
-            $_total_date += $loop_table[$mon] - $day;
-
-            $_day_time = $hr * $_hour_power + $min * $_min_power + $sec;
-            $_day_time = $_day_power - $_day_time;
-            $ret = -( $_total_date * $_day_power + $_day_time - $gmt_different);
-            if ($ret < -12220185600) $ret += 10*86400; // if earlier than 5 Oct 1582 - gregorian correction
-            else if ($ret < -12219321600) $ret = -12219321600; // if in limbo, reset to 15 Oct 1582.
-        }
-        //print " dmy=$day/$mon/$year $hr:$min:$sec => " .$ret;
-        return $ret;
-    }
-
-    /**
-    Fix 2-digit years. Works for any century.
-    Assumes that if 2-digit is more than 30 years in future, then previous century.
-     */
-    public function adodb_year_digit_check($y)
-    {
-        if ($y < 100) {
-
-            $yr = (integer) date("Y");
-            $century = (integer) ($yr /100);
-
-            if ($yr%100 > 50) {
-                $c1 = $century + 1;
-                $c0 = $century;
-            } else {
-                $c1 = $century;
-                $c0 = $century - 1;
-            }
-            $c1 *= 100;
-            // if 2-digit year is less than 30 years in future, set it to this century
-            // otherwise if more than 30 years in future, then we set 2-digit year to the prev century.
-            if (($y + $c1) < $yr+30) $y = $y + $c1;
-            else $y = $y + $c0*100;
-        }
-        return $y;
-    }
-
-    /**
-    Checks for leap year, returns true if it is. No 2-digit year check. Also
-    handles julian calendar correctly.
-     */
-    public function _adodb_is_leap_year($year)
-    {
-        if ($year % 4 != 0) return false;
-
-        if ($year % 400 == 0) {
-            return true;
-            // if gregorian calendar (>1582), century not-divisible by 400 is not leap
-        } else if ($year > 1582 && $year % 100 == 0 ) {
-            return false;
-        }
-
-        return true;
-    }
-
-    /**
-    get local time zone offset from GMT. Does not handle historical timezones before 1970.
-     */
-    public function adodb_get_gmt_diff($y,$m,$d)
-    {
-        static $TZ,$tzo;
-        global $ADODB_DATETIME_CLASS;
-
-        if (!defined('ADODB_TEST_DATES')) $y = false;
-        else if ($y < 1970 || $y >= 2038) $y = false;
-
-        if ($ADODB_DATETIME_CLASS && $y !== false) {
-            $dt = new \DateTime();
-            $dt->setISODate($y,$m,$d);
-            if (empty($tzo)) {
-                $tzo = new \DateTimeZone(date_default_timezone_get());
-                #	$tzt = timezone_transitions_get( $tzo );
-            }
-            return -$tzo->getOffset($dt);
-        } else {
-            if (isset($TZ)) return $TZ;
-            $y = date('Y');
-            /*
-            if (function_exists('date_default_timezone_get') && function_exists('timezone_offset_get')) {
-                $tzonename = date_default_timezone_get();
-                if ($tzonename) {
-                    $tobj = new DateTimeZone($tzonename);
-                    $TZ = -timezone_offset_get($tobj,new DateTime("now",$tzo));
-                }
-            }
-            */
-            if (empty($TZ)) $TZ = mktime(0,0,0,12,2,$y) - gmmktime(0,0,0,12,2,$y);
-        }
-        return $TZ;
-    }
 
 }
